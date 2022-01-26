6b28e2ee36921bfdd193380c05fd72b0cbe0f416
hyesun <halla116@naver.com>
2022-01-26 21:56:59 +0900

    Auditing

--- src/main/java/com/shop/config/AuditorConfig.java
+++ src/main/java/com/shop/config/AuditorConfig.java
@@ -1,10 +1,18 @@
 package com.shop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing //JPA Auditing 기능 활성화
 public class AuditorConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        //등록자와 수정자를 처리하는 AuditorAware 빈으로 등록
+        return new AuditorAwareImpl();
+    }
 
 }
